88b9824983d949b6d106eecd9f1b13db8e973ceb
Evgeny Golokha <evgeny@example.com>
2025-12-04 23:13:14 +0500

    Add hunter method and test

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,16 +1,26 @@
 package plaindoll;
 
-public class Welcomer{
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
+public class Welcomer {
+
+    public String sayWelcome() {
+        return "Welcome home, good hunter. What is it your desire?";
+    }
+
+    public String sayFarewell() {
+        return "Farewell, good hunter. May you find your worth in waking world.";
+    }
+
+    public String sayNeedGold() {
+        return "Not enough gold";
+    }
+
+    public String saySome() {
+        return "something in the way";
+    }
+
+    // Новый метод по заданию
+    public String hunter(String name) {
+        return "Hunting for " + name + "!";
+    }
 }
+
